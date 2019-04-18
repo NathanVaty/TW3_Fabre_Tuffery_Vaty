be5d9736a5aff62f9152462e29f738cd8791dcd9
pierrick fabre <pierrick.fabre.pro@gmail.com>
2019-04-18 10:10:24 +0200

    bug sur le for

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -18,32 +18,41 @@ export class CarteComponent implements OnInit {
   ngOnInit() {
     /* Création de la carte */
     /* Coordonnée sur un zoom de la france */
-    var myMap = L.map('mapId').setView([43.9211, 2.1377], 1);
+    var mapFestival = L.map('mapId').setView([47.391, 1.000],5);
 
     L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution:'Carte des festivals',
       maxZoom: 20,
-    }).addTo(myMap);
+    }).addTo(mapFestival);
 
     /* Appel de la fonction getDonnees dans un tableau de festival */
-    var festivalList = this.data.getDonnees();
+    let festivalList = this.data.getDonnees();
 
     /* Création d'un marqueur */
     const myMark = L.icon({
-      iconUrl:'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+      iconUrl :'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
     });
 
     console.log("ajout des festivals sur la carte");
+    console.log("avant le for");
     /* Pour chaque festival on ajoute un marqueur */
-    for (var i in festivalList){
+    for (let i of festivalList){
+      console.log("coucou je suis dans le for");
+      console.log(festivalList[i].coordonnees_insee);
       /* Création de variable pour le marqueur */
-      var latitude = festivalList[i].coordonnees_insee[0];
-      var longitude = festivalList[i].coordonnees_insee[1];
-      var name = festivalList[i].nom_de_la_manifestation;
+      //var latitude = festivalList[i].coordonnees_insee[0];
+      //var longitude = festivalList[i].coordonnees_insee[1];
+      //var name = festivalList[i].nom_de_la_manifestation;
 
       /* Affichage des marqueur */
-      L.marker([latitude, longitude], {icon: myMark}).bindPopup(name).addTo(myMap);
+      L.marker([festivalList[i].coordonnees_insee[0], festivalList[i].coordonnees_insee[1]], {icon: myMark})
+      .addTo(mapFestival)
+      .bindPopup(festivalList[i].nom_de_la_manifestation);
+      //L.marker([latitude, longitude], {icon: myMark}).bindPopup(name).addTo(myMap);
     }
+
+    console.log("on a finis la generation de la carte");
+
   }
 
 }
